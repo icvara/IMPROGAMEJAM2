c8547eeb48a62d9c52375527f31ebb46bd9b4355
Icvara <icvara.aor@gmail.com>
2025-07-20 19:36:29 +0100

    more boss

--- impro_gamejam_2/Scripts/monster_1.gd
+++ impro_gamejam_2/Scripts/monster_1.gd
@@ -6,7 +6,7 @@ var value_dmg = 0
 func attack(player):
 	pass
 	$AnimatedSprite2D.play("attack")
-	value_dmg = [0,2,2,4].pick_random()
+	value_dmg = [8,4,0,4].pick_random()
 	Save.Health -= value_dmg
 
 	await get_tree().create_timer(1.8).timeout
